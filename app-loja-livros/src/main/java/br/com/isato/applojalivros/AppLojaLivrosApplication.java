0049d275017b7d450433c51cf963e55842372a01
igoryouiti <igoryouiti@hotmail.com>
2023-08-17 15:48:02 -0300

    start backend project

---
+++ app-loja-livros/src/main/java/br/com/isato/applojalivros/AppLojaLivrosApplication.java
@@ -0,0 +1,13 @@
+package br.com.isato.applojalivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppLojaLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppLojaLivrosApplication.class, args);
+	}
+
+}
---
+++ app-loja-livros/src/test/java/br/com/isato/applojalivros/AppLojaLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.isato.applojalivros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppLojaLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
